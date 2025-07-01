fd36a281e3121eb71247f182c0de43080e6b5526
Sufyan MQ <sufyanmq06@gmail.com>
2025-07-02 01:14:51 +0200

    Ejercicio1 Done

--- Ejercicio1/main.js
+++ Ejercicio1/main.js
@@ -16,3 +16,38 @@ Esta sería la URL final (la que deberéis utilizar para vuestra petición):
 
 https://thronesapi.com/api/v2/Characters
 */
+
+const select = document.getElementById("character-list");
+
+const img = document.getElementById("character-image");
+
+const obtenercharacter = async () => {
+  try {
+    const res = await fetch("https://thronesapi.com/api/v2/Characters");
+    const data = await res.json();
+
+    data.forEach((character) => {
+      const option = document.createElement("option");
+      option.value = character.imageUrl;
+      option.textContent = character.fullName;
+
+      select.appendChild(option);
+    });
+  } catch (error) {
+    console.error("Error al cargar personajes:", error);
+  }
+};
+
+select.addEventListener("change", (e) => {
+  const selectedImage = e.target.value;
+
+  if (selectedImage) {
+    img.src = selectedImage;
+
+    img.style.display = "block";
+  } else {
+    img.style.display = "none";
+  }
+});
+
+obtenercharacter();
