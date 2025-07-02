49fe76aa4de0a063a6f05d96d5fc7262f1796b8f
Sufyan MQ <sufyanmq06@gmail.com>
2025-07-02 03:05:46 +0200

    Ejercicio2 Done

--- Ejercicio2/main.js
+++ Ejercicio2/main.js
@@ -16,3 +16,31 @@ URL: https://pokeapi.co/api/v2/pokemon/1
 
 Tened en cuenta que esta URL se refiere al pokemon número 1, que es bulbasaur, debemos hallar la manera de con una url similar ir consiguiendo pokemons aleatorios dentro de unos límites 
 */
+
+const img = document.querySelector(".random-image");
+const nameDiv = document.getElementById("pokemon-name");
+
+const obtenerPokemonAleatorio = async () => {
+  const idAleatorio = Math.floor(Math.random() * 151) + 1;
+  const url = `https://pokeapi.co/api/v2/pokemon/${idAleatorio}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const imagen = data.sprites.other["official-artwork"].front_default;
+    const nombre = data.name;
+
+    if (imagen) {
+      img.src = imagen;
+      img.alt = nombre;
+      nameDiv.textContent = nombre;
+    } else {
+      img.alt = "Imagen no disponible";
+    }
+  } catch (error) {
+    console.error("Error al obtener el Pokémon:", error);
+  }
+};
+
+obtenerPokemonAleatorio();
